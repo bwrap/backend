9bb32cfc621020a9a23417d6fd7c93c9657c8fde
armando <armandomendivil.m@gmail.com>
2017-07-22 18:07:24 -0700

    Add bluebird to handle promises with mongo

--- dl/environment.js
+++ dl/environment.js
@@ -3,6 +3,8 @@
 const mongoose = require('mongoose');
 const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/test';
 
+mongoose.Promise = require('bluebird');
+
 mongoose.connect(MONGO_URL);
 
 module.exports = mongoose;
