672ee7fa0e847c9cff302fe5430f129a07601881
armando <armandomendivil.m@gmail.com>
2017-08-20 15:30:50 -0700

    Add docker-compose

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,12 @@
 FROM node:8
 EXPOSE 9000
 
+# Create app directory
+RUN mkdir -p /app
+
+# Install new node modules
+RUN npm install -g nodemon
+
 WORKDIR /app
 
 ADD package.json /app/
--- dl/environment.js
+++ dl/environment.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const mongoose = require('mongoose');
-const MONGO_URL = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/test';
+const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/test';
 
 mongoose.Promise = require('bluebird');
 
